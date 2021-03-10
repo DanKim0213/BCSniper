510bca67a5752f595ec868141056df2056c0cbeb
Dan <dankim0213@gmail.com>
2021-03-10 17:54:36 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
+/* eslint-disable no-console */
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Applictaion running on 3000');
-});
+const port = process.env.PORT || 3001;
 
-app.get('/sniper', (req, res) => {
-  // res.send('OK');
-  res.sendFile(`${__dirname}/sniper.html`);
+app.listen(port, () => {
+  console.log(`Applictaion running on ${port}`);
 });
-// console.log('"Hello World!"');
+app.set('view engine', 'pug');
+app.set('views', path.join(`${__dirname}`, 'views'));
+
+if (process.env.DEV_ENV === 'develop') {
+  app.use(morgan('dev'));
+}
+
+app.get('/sniper', async (req, res) => {
+  // res.sendFile(`${__dirname}/sniper.html`);
+  try {
+    const result = await axios.get(
+      'https://api.coindesk.com/v1/bpi/currentprice/KRW.json'
+    );
+    // const data = result.data.bpi.USD.rate;
+    const data = result.data.bpi;
+    console.log(data);
+    const responseData = { val: data.KRW.rate };
+    res.status(200).render('layout', responseData); // data
+  } catch (err) {
+    console.log('Error occurred...', err);
+  }
+});
