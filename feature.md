8249e40eec4917188e4c042dd633d2b8106ee9a7
dan <dankim0213@gmail.com>
2021-04-28 19:05:06 +0900

    init feature 13

---
+++ feature.md
@@ -0,0 +1,8 @@
+## Feature
+- Test 
+
+## Process
+- e2e
+
+## TODO
+- unlock bundle.js on .gitignore
--- test/e2e.js
+++ test/e2e.js
@@ -1,45 +1,92 @@
-/* eslint-disable node/no-unpublished-require */
-/* eslint-disable no-undef */
+/* eslint-disable */
 const { expect } = require('chai');
 const puppeteer = require('puppeteer');
 
-describe('End to End testing', () => {
+describe.skip('End to End Testing1', function () {
+  this.timeout(5000);
   let browser;
   let page;
-
-  before('Set up ', async () => {
+  
+  beforeEach('Set up', async () => {
+    // browser = await puppeteer.launch({ headless: false, slowMo: 250 });
     browser = await puppeteer.launch();
     page = await browser.newPage();
+    await page.goto('http://localhost:3000');
   });
 
-  after('Close browser', async () => {
+  afterEach('Close browser', async () => {
     await page.close();
     await browser.close();
   });
 
-  it.skip('open brower and check "Hello World"', async () => {
-    await page.goto('http://localhost:3000/sniper');
-    const content = await page.evaluate(() => {
-      const title = document.querySelector('#hello').innerHTML;
-      return title;
-    });
-    // console.log(content);
-    expect(content).to.equal('Hello World!');
+  it('click the login button', async () => {
+    await page.$eval('#login', btn => btn.click());
+    await page.waitForSelector('.login-form', { timeout: 5000 });
+
+    const loginStr = await page.$eval('.login-form', e => e.querySelector('h2').innerHTML);
+    expect(loginStr).to.be.equal('Log into your account');
   });
 
-  it('Sniper joins until closed', () => {});
+  it('click "give a shot" button', async () => {
+    await page.$eval('a.btn.btn--green.span-all-rows', btn => btn.click());
+    await page.waitForSelector('.login-form', { timeout: 5000 });
 
-  it('Sniper makes a higher bid but loses', () => {});
+    const loginStr = await page.$eval('.login-form', e => e.querySelector('h2').innerHTML);
+    expect(loginStr).to.be.equal('Log into your account');
+  });
+});
 
-  it('Sniper wins an bitcoin by bidding higher', () => {});
+describe('End to End testing2', () => {
+  let browser;
+  let page;
 
-  it('Sniper sells an bitcoin when the price meets the upper bound', () => {});
+  beforeEach('Set up', async function () {
+    // browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+    await page.$eval('#login', btn => btn.click());
+    await page.waitForSelector('.login-form', { timeout: 5000 });
 
-  it('Sniper sells an bitcoin when the price meets the lower bound', () => {});
+    await page.type('#email', 'newhacker@hacker.com');
+    await page.type('#password', 'pass1234');
+    await page.$eval('button', btn => btn.click());
+  });
 
-  it('sniper bids for multiple items', () => {});
+  afterEach('Close browser', async () => {
+    await browser.close();
+  });
 
-  it('Sniper loses an bitcoin when the price is too high', () => {});
+  it('Sniper is logged in', async function () {
+    this.timeout(10000); // Alerting 'success' takes over 2000 ms
+    await page.waitForSelector('a.nav__el.nav__el--logout'); // wait for login 
+    
+    const logout = await page.$eval('a.nav__el.nav__el--logout', e => e.innerHTML);
+    expect(logout).to.be.equal('Log out'); 
+  });
 
-  it('Sniper stops when error occurred', () => {});
+  it('click "shoot now" button', async function () {
+    this.timeout(10000);
+    await page.waitForSelector('a.nav__el.nav__el--logout');
+    await page.$eval('#home', btn => btn.click());
+    await page.$eval('button.btn.btn--green.span-all-rows', btn => btn.click());
+    await page.waitForSelector('.item-container', { timeout: 5000 });
+
+    const cntLen = await page.$eval('.item-container', e => e.querySelectorAll('.section-cta').length);
+    console.log(cntLen); expect(cntLen).to.be.lte(0);
+  });
+
+  it.skip('Sniper buys an item and sells the item immediately', () => {});
+
+  it.skip('Sniper buys an item and sells the item when won', () => {});
+
+  it.skip('Sniper buys an item and sells the item when lost', () => {});
+
+  it.skip('Sniper buys an item and sells the item when due to the date', () => {});
+
+  it.skip('sniper buys multiple items', () => {});
+
+  it.skip('Sniper cannot buy an item when the price is too high', () => {});
+
+  it.skip('Sniper stops when error occurred', () => {});
 });
